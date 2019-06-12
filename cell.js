1e3b8cc154bf2bf559fe0eef6d541f729078f7f7
DeadlyLaser <lucabaggi.dev@gmail.com>
2019-06-12 12:02:13 +0200

    cleaned  code and added reveal graphics and  logic

--- cell.js
+++ cell.js
@@ -1,22 +1,48 @@
-class Cell 
-{
-    constructor(x,y,w)
-    {
-    this.mined = true;
-    this.reveled = true;
-    this.x = x;
-    this.y = y;
-    this.w = w
+class Cell {
+    constructor(x, y, w) {
+        this.mined = true;
+        this.revealed = false;
+        this.x = x;
+        this.y = y;
+        this.w = w;
     }
-    
-    Show() 
-    {
-     rect(this.x,this.y,this.w,this.w);
-     if (this.mined)
-     {
-         ellipse(this.x + (this.w/2),this.y + (this.w/2),this.w/2,this.w/2);
+    // draw the cell on the screen
+    Show() {
+        stroke(0);
+        noFill();
+        rect(this.x, this.y, this.w, this.w);
 
-     }
+        if (this.revealed) {
+            if (this.mined) {
+                fill(150);
+                rect(this.x, this.y, this.w, this.w);
+                fill(255);
+                ellipse(this.x + (this.w / 2), this.y + (this.w / 2), this.w / 2, this.w / 2);
+            }
+
+            else {
+                fill(200);
+                rect(this.x, this.y, this.w, this.w);
+            }
+        }
+    }
+    // return true if the point given is contained in the cell 
+    OnTheCell(x, y) {
+        return (x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.w);
+    }
+
+    //reveal the cell
+    Reveal(){
+        this.revealed = true;
     }
 }
-        
+
+
+
+
+
+
+
+
+
+
--- sketch.js
+++ sketch.js
@@ -1,45 +1,53 @@
-//function to create a 2d array for the grid
 
-function Create2DArray(rows, cols){
-  let arr = new Array (cols);
-  for (let i = 0; i < cols; i++){
-    arr[i] = new Array (rows);
-  }
-  return arr;
-  }
-  
 //global variables
 var rows = 0;
 var cols = 0;
 var canvasSize = 500
 var cellW = 50;
-
 var grid;
 
 function setup() {
   createCanvas(canvasSize, canvasSize);
   rows = floor(height / cellW);
   cols = floor(width / cellW);
-  grid = Create2DArray(rows,cols);
- 
+  grid = Create2DArray(rows, cols);
 
-  for (let i = 0; i < cols; i++){
-    for(let j = 0; j < rows; j++){
-      grid[i][j] = new Cell(i * cellW, j * cellW,cellW);
-      console.log(grid[i][j]);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j] = new Cell(i * cellW, j * cellW, cellW);
     }
   }
-
 }
 
+// ceck if the mouse clicked on a cell, if so it reveals it.
+function mousePressed() {
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      if (grid[i][j].OnTheCell(mouseX, mouseY)) {        
+        grid[i][j].Reveal();
+      }
+    }
+  }
+}
+
+
+
 function draw() {
-  background(220);
-  for (let i = 0; i < cols; i++){
-    for(let j = 0; j < rows; j++){
+  background(255);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
       grid[i][j].Show();
     }
   }
-
 }
-  
-  
+
+//function to create a 2d array for the grid
+
+function Create2DArray(rows, cols) {
+  let arr = new Array(cols);
+  for (let i = 0; i < cols; i++) {
+    arr[i] = new Array(rows);
+  }
+  return arr;
+}
+
